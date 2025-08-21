0b3db26f0a03e085b244a1f267150eada4bd31d4
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-08-21 18:02:00 -0500

    Adjust the responsiveness of the login screen

--- NoteMark/app/src/main/java/com/joshayoung/notemark/auth/presentation/log_in/LogInScreen.kt
+++ NoteMark/app/src/main/java/com/joshayoung/notemark/auth/presentation/log_in/LogInScreen.kt
@@ -1,6 +1,5 @@
 package com.joshayoung.notemark.auth.presentation.log_in
 
-import android.content.res.Configuration
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,27 +10,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.window.core.layout.WindowWidthSizeClass
+import com.joshayoung.notemark.core.design.theme.EyeIcon
+import com.joshayoung.notemark.core.design.theme.NoteMarkTheme
 import com.joshayoung.notemark.core.presentation.ObserveAsEvents
 import com.joshayoung.notemark.core.presentation.components.NoteMarkButton
 import com.joshayoung.notemark.core.presentation.components.NoteMarkTextField
 import com.joshayoung.notemark.core.presentation.components.TextFieldType
-import com.joshayoung.notemark.core.design.theme.EyeIcon
-import com.joshayoung.notemark.core.design.theme.NoteMarkTheme
 import com.joshayoung.notemark.core.utils.DeviceConfiguration
 import org.koin.androidx.compose.koinViewModel
 
@@ -58,39 +59,126 @@ fun LoginScreenRoot(
         }
     }
 
-    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
-    val deviceConfiguration = DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
 
-    when(deviceConfiguration) {
-        DeviceConfiguration.MOBILE_PORTRAIT -> {
-            LoginScreenPortrait(
-                state = viewModel.state,
-                onAction = { action ->
-                    viewModel.onAction(action)
-                }
-            )
+    LoginScreen(
+        state = viewModel.state,
+        onAction = { action ->
+            viewModel.onAction(action)
         }
-        DeviceConfiguration.MOBILE_LANDSCAPE -> {
-            LoginScreenLandscape(
-                state = viewModel.state,
-                onAction = { action ->
-                    viewModel.onAction(action)
+    )
+}
+
+    @Composable
+    fun LoginScreen(
+        state: LoginState,
+        onAction: (LoginAction) -> Unit
+    ) {
+        val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+        val deviceConfiguration = DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
+
+        val loginModifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+
+        when(deviceConfiguration) {
+            DeviceConfiguration.MOBILE_PORTRAIT -> {
+                Column(
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .padding(top = 20.dp)
+                ) {
+                    Column(
+                        modifier = loginModifier
+                            .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+                            .padding(horizontal = 16.dp, vertical = 24.dp)
+                    ) {
+                        LoginHeader(
+                            modifier = Modifier
+                                .padding(top = 40.dp)
+                        )
+                        LoginContent(
+                            state = state,
+                            onAction = { action ->
+                                onAction(action)
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
                 }
-            )
+            }
+        DeviceConfiguration.MOBILE_LANDSCAPE -> {
+            Row(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .padding(top = 20.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                        .background(Color.White)
+                        .fillMaxSize()
+                        .padding(20.dp)
+                    ,horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    LoginHeader(
+                        modifier = Modifier
+                            .weight(1f)
+                    )
+                    LoginContent(
+                        state = state,
+                        onAction = { action ->
+                            onAction(action)
+                        },
+                        modifier = Modifier
+                            .weight(1f)
+                            .verticalScroll(rememberScrollState())
+                    )
+                }
+            }
         }
         DeviceConfiguration.TABLET_PORTRAIT,
         DeviceConfiguration.TABLET_LANDSCAPE,
         DeviceConfiguration.DESKTOP -> {
-
+            Column(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .padding(top = 20.dp)
+            ) {
+                Column(
+                    modifier = loginModifier
+                        .verticalScroll(rememberScrollState())
+                        .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+                        .padding(vertical = 48.dp),
+                    verticalArrangement = Arrangement.spacedBy(32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LoginHeader(
+                        alignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .widthIn(max = 540.dp)
+                    )
+                    LoginContent(
+                        modifier = Modifier
+                            .widthIn(max = 540.dp)
+                        ,state = state,
+                        onAction = { action ->
+                            onAction(action)
+                        }
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
 fun LoginHeader(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    alignment: Alignment.Horizontal = Alignment.Start,
 ) {
-    Column(modifier = modifier
+    Column(modifier = modifier,
+        horizontalAlignment = alignment
     ) {
         Text("Log In", modifier = Modifier,
             style = MaterialTheme.typography.titleLarge
@@ -102,112 +190,50 @@ fun LoginHeader(
 }
 
 @Composable
-fun LoginScreenPortrait(
-    state: LoginState,
-    onAction: (LoginAction) -> Unit
-) {
-    Column(
-        modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.primary)
-            .padding(top =10.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(20.dp)
-    )
-    {
-        Column(
-            modifier = Modifier
-                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                .background(Color.White)
-                .padding(20.dp)
-        ) {
-            LoginHeader(
-                modifier = Modifier
-                    .padding(top = 40.dp)
-            )
-            LoginContent(
-                state = state,
-                onAction = onAction
-            )
-        }
-    }
-}
-
-@Composable
-fun LoginScreenLandscape(
-    state: LoginState,
-    onAction: (LoginAction) -> Unit
-) {
-    Row(modifier = Modifier
-        .background(color = MaterialTheme.colorScheme.primary)
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(top = 10.dp)
-                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                .background(Color.White)
-                .fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            LoginHeader(
-                modifier = Modifier
-                    .padding(40.dp)
-            )
-            LoginContent(
-                state = state,
-                onAction = onAction,
-                modifier = Modifier
-                    .size(400.dp)
-                    .padding(20.dp)
-            )
-        }
-    }
-}
-
-@Composable
 fun LoginContent(
     state: LoginState,
     onAction: (LoginAction) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier
 ) {
     Column(
         modifier = modifier
-            .fillMaxSize()
     ) {
-    NoteMarkTextField(state = state.username, label = "Username", hint = "john.doe@example.com")
-    NoteMarkTextField(
-        modifier = Modifier,
-        state = state.password,
-        label = "Password",
-        icon = EyeIcon,
-        hint = "Password",
-        type = TextFieldType.Password
-    )
-    NoteMarkButton(text = "Log In", isEnabled = state.formFilled,
-        isLoading = state.isLoggingIn
-    ) {
-        onAction(LoginAction.OnLoginClick)
+        NoteMarkTextField(state = state.username, label = "Username", hint = "john.doe@example.com")
+        NoteMarkTextField(
+            modifier = Modifier,
+            state = state.password,
+            label = "Password",
+            icon = EyeIcon,
+            hint = "Password",
+            type = TextFieldType.Password
+        )
+        NoteMarkButton(text = "Log In", isEnabled = state.formFilled,
+            isLoading = state.isLoggingIn
+        ) {
+            onAction(LoginAction.OnLoginClick)
+        }
+        Text(
+            "Don't have an account?", modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onAction(LoginAction.DontHaveAccount)
+            }, textAlign = TextAlign.Center
+        )
     }
-    Text(
-        "Don't have an account?", modifier = Modifier
-        .fillMaxWidth()
-        .clickable {
-            onAction(LoginAction.DontHaveAccount)
-        }, textAlign = TextAlign.Center
-    )
-}
 }
 
 @Composable
+@Preview(showBackground = true)
 @Preview(
     showBackground = true,
     widthDp = 840, heightDp = 360)
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 800, heightDp = 1280)
 fun LoginScreenPreview() {
     NoteMarkTheme {
-        LoginScreenLandscape(state = LoginState(),
-            onAction = {}
-        )
-        LoginScreenPortrait(state = LoginState(),
+        LoginScreen(
+            state = LoginState(),
             onAction = {}
         )
     }
