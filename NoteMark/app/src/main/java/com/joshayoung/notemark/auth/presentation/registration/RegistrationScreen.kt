dfcbbefef983a6d1a960eedf1c3fb2e1f5b008e0
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-08-23 15:51:55 -0500

    Correct Responsiveness for registration page

--- NoteMark/app/src/main/java/com/joshayoung/notemark/auth/presentation/registration/RegistrationScreen.kt
+++ NoteMark/app/src/main/java/com/joshayoung/notemark/auth/presentation/registration/RegistrationScreen.kt
@@ -10,22 +10,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,6 +41,7 @@ import com.joshayoung.notemark.core.presentation.ObserveAsEvents
 import com.joshayoung.notemark.core.presentation.components.NoteMarkButton
 import com.joshayoung.notemark.core.presentation.components.NoteMarkTextField
 import com.joshayoung.notemark.core.presentation.components.TextFieldType
+import com.joshayoung.notemark.core.utils.DeviceConfiguration
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -74,48 +79,80 @@ fun RegistrationScreen(
     errorMessage: List<String?>?,
     onnAction: (RegistrationAction) -> Unit,
 ) {
-    val configuration = LocalConfiguration.current
-    val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    val deviceConfiguration = DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
 
-    if (!isPortrait) {
-        Row(
-            modifier = Modifier
+    when (deviceConfiguration) {
+        DeviceConfiguration.MOBILE_PORTRAIT -> {
+            Column(modifier = Modifier
                 .background(color = MaterialTheme.colorScheme.background)
-        ) {
-            Row(
-                modifier =
-                    Modifier
-                        .padding(top = 30.dp)
+                .padding(top = 30.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
                         .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
                         .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
-                        .fillMaxSize(),
-            ) {
-                Header(modifier = Modifier.weight(1f))
-                Form(
-                    errorMessage,
-                    state,
-                    onnAction,
-                    modifier = Modifier.weight(1f),
-                )
+                        .padding(horizontal = 20.dp)
+                ) {
+                    Header()
+                    Form(
+                        errorMessage,
+                        state,
+                        onnAction,
+                    )
+                }
             }
         }
-    } else {
-        Column(modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.background)
-            .padding(top = 30.dp)
-        ) {
-            Column(
+        DeviceConfiguration.MOBILE_LANDSCAPE -> {
+            Row(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                    .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+                    .background(color = MaterialTheme.colorScheme.background)
             ) {
-                Header()
-                Form(
-                    errorMessage,
-                    state,
-                    onnAction,
-                )
+                Row(
+                    modifier =
+                        Modifier
+                            .padding(top = 30.dp)
+                            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                            .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+                            .fillMaxSize(),
+                ) {
+                    Header(modifier = Modifier.weight(1f))
+                    Form(
+                        errorMessage,
+                        state,
+                        onnAction,
+                        modifier = Modifier.weight(1f)
+                        .padding(bottom = 30.dp, end = 10.dp)
+                    )
+                }
+            }
+        }
+        DeviceConfiguration.TABLET_PORTRAIT,
+        DeviceConfiguration.TABLET_LANDSCAPE,
+        DeviceConfiguration.DESKTOP, -> {
+            Column(modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.background)
+                .padding(top = 30.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                        .background(color = MaterialTheme.colorScheme.surfaceContainerLowest),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Header(modifier = Modifier
+                        .widthIn(max = 540.dp)
+                    )
+                    Form(
+                        errorMessage,
+                        state,
+                        onnAction,
+                        modifier = Modifier
+                            .widthIn(max = 540.dp),
+                    )
+                }
             }
         }
     }
@@ -128,73 +165,71 @@ private fun Form(
     onnAction: (RegistrationAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-        Column(
-            modifier
-                .padding(top = 20.dp)
-                .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
-                .verticalScroll(rememberScrollState())
-                .fillMaxWidth()
-                .padding(vertical = 10.dp, horizontal = 10.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            DisplayErrorList(
-                modifier =
-                    Modifier
-                        .weight(1f),
-                errorMessage,
-            )
+    Column(
+        modifier
+            .padding(top = 20.dp)
+            .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        DisplayErrorList(
+            modifier =
+                Modifier
+                    .weight(1f),
+            errorMessage,
+        )
 
-            NoteMarkTextField(
-                state = state.username,
-                label = "Username",
-                hint = "John.doe",
-                helperText = "Use between 3 and 20 characters for your username",
-            )
-            if (state.usernameError != "") {
-                Text(text = state.usernameError, color = MaterialTheme.colorScheme.error)
-            }
-            NoteMarkTextField(state = state.email, label = "Email", hint = "john.doe@example.com")
-            if (state.emailError != "") {
-                Text(text = state.emailError, color = MaterialTheme.colorScheme.error)
-            }
-            NoteMarkTextField(
-                state = state.password,
-                label = "Password",
-                icon = EyeIcon,
-                hint = "Password",
-                helperText = "Use 8+ characters with a number or symbol for better security",
-                type = TextFieldType.Password,
-            )
-            if (state.passwordError != "") {
-                Text(text = state.passwordError, color = MaterialTheme.colorScheme.error)
-            }
-            NoteMarkTextField(
-                state = state.repeatedPassword,
-                label = "Repeat Password",
-                icon = EyeIcon,
-                hint = "Repeat Password",
-                type = TextFieldType.Password,
-            )
-            if (state.passwordEqualityError != "") {
-                Text(text = state.passwordEqualityError, color = MaterialTheme.colorScheme.error)
-            }
-            NoteMarkButton(
-                text = "Create Account",
-                isEnabled = state.canRegister && !state.isRegistering,
-                isLoading = state.isRegistering,
-            ) {
-                onnAction(RegistrationAction.OnRegisterClick)
-            }
-            Text(
-                stringResource(R.string.have_account),
-                style = MaterialTheme.typography.titleSmall,
-                modifier =
-                    Modifier
-                        .clickable {
-                            onnAction(RegistrationAction.AlreadyAccount)
-                        }.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-            )
+        NoteMarkTextField(
+            state = state.username,
+            label = "Username",
+            hint = "John.doe",
+            helperText = "Use between 3 and 20 characters for your username",
+        )
+        if (state.usernameError != "") {
+            Text(text = state.usernameError, color = MaterialTheme.colorScheme.error)
+        }
+        NoteMarkTextField(state = state.email, label = "Email", hint = "john.doe@example.com")
+        if (state.emailError != "") {
+            Text(text = state.emailError, color = MaterialTheme.colorScheme.error)
+        }
+        NoteMarkTextField(
+            state = state.password,
+            label = "Password",
+            icon = EyeIcon,
+            hint = "Password",
+            helperText = "Use 8+ characters with a number or symbol for better security",
+            type = TextFieldType.Password,
+        )
+        if (state.passwordError != "") {
+            Text(text = state.passwordError, color = MaterialTheme.colorScheme.error)
+        }
+        NoteMarkTextField(
+            state = state.repeatedPassword,
+            label = "Repeat Password",
+            icon = EyeIcon,
+            hint = "Repeat Password",
+            type = TextFieldType.Password,
+        )
+        if (state.passwordEqualityError != "") {
+            Text(text = state.passwordEqualityError, color = MaterialTheme.colorScheme.error)
+        }
+        NoteMarkButton(
+            text = "Create Account",
+            isEnabled = state.canRegister && !state.isRegistering,
+            isLoading = state.isRegistering,
+        ) {
+            onnAction(RegistrationAction.OnRegisterClick)
+        }
+        Text(
+            stringResource(R.string.have_account),
+            style = MaterialTheme.typography.titleSmall,
+            modifier =
+                Modifier
+                    .clickable {
+                        onnAction(RegistrationAction.AlreadyAccount)
+                    }.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
